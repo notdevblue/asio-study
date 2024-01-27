cde62569ae7afb7b3e74f66a52fb3f1e54cb7c8a
HanPCGentoo <wooyeophan@gmail.com>
2024-01-27 23:31:11 +0900

    CMake

--- NetClient/SimpleClient.cpp
+++ NetClient/SimpleClient.cpp
@@ -1,2 +1,12 @@
 // https://youtu.be/2hNdkYInj4g?si=bqJr0AssVWlSPhq7&t=2172
 // cmake 대충 보고 와야 할듯. 영샹이 dependency 관련 내용임
+
+#include <iostream>
+#include "han_net.h"
+
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
