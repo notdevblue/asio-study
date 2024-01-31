39daf09ed319be922cb29fa1e195df25d93c669a
HanPCGentoo <wooyeophan@gmail.com>
2024-01-31 23:17:36 +0900

    파트1 다함!

--- NetClient/SimpleClient.cpp
+++ NetClient/SimpleClient.cpp
@@ -12,7 +12,58 @@ enum class CustomMsgTypes : uint32_t { // 그리고 uint32_t 타입으로 구현
     MovePlayer
 };
 
+// 클라이언트 인터페이스를 상속받아서 이 애플리케이션 전용의 클라이언트 클레스를 만들 수 있음
+// 이 시점에서 템플릿은 사라지게 됨
+// client_interface 클레스에 CustomMsgTypes 를 전달함
+class CustomClient : public han::net::client_interface<CustomMsgTypes>
+{
+public:
+    // Connect, Disconnect, IsConnected 함수는 이미 재공됨
+    // 바로 실제 로직을 구현할 수 있음
+
+    // 서버에게 특정한 위치에 총알을 발사했다고 보내고 싶다고 함
+    bool FireBullet(float x, float y) {
+        // x, y 를 받고
+        // 메세지를 만듬
+        han::net::message<CustomMsgTypes> msg;
+        msg.header.id = CustomMsgTypes::FireBullet;
+
+        // 데이터 넣음
+        msg << x << y;
+        Send(msg);
+    }
+};
+
 int main() {
+
+    // 메인 게임 어딘가에서 커스텀 클라이언트 인스턴트를 생성
+    CustomClient c;
+    // 특정한 포트와 주소를 넣음
+    c.Connect("community.onelonecoder.com", 60000);
+
+    // 서버에게 총알이 발사됬다고 전해야 할 때는 아레의 함수를 호출함
+    c.FireBullet(2.0f, 5.0f);
+
+    // 현재 상태로는 컴파일되지 않음
+    // 아직 대부분은 만들어지지 않았음
+    // 그리도 대부분의 것들을 구현하지도 않음
+    // 하지만 이 단계에서 우리의 프레임워크는 복잡한 것들,
+    // 예를 들어 클라이언트와 서버 간의 관계 등을
+    // 상당히 유연하게 줄였고, 간단하고 사용하기 쉽게 만들었음
+
+    // 이건 항상 1단계의 경우임
+    // 파트2에는 클라이언트, 서버, 연결을 전부 구현하고, 메세지를 실제로 이동해 볼 것
+    // 우리가 미리 만들어둔 것은, 트랜잭션을 위한 멋진 확정된 메세징 시스템 프레임워크와 큐임
+    // 지금 시점에선 우리가 해결해 나가야 하는 문제들을 아주 높은 레벨로만 봤음
+    // 이제 우리는 타이밍이나 연결을 강하게 유지하는 일, 메세지 순서 보장에 대처할 것
+    // 네트워크 끊김 또는, 클라이언트가 예측되지 않은 순간에 접속을 끊으면 어떻게 대처할 것인지
+
+    // 그리고 진도가 잘 나가면, 로그인 시스템을 어떻게 구현하는지 알아볼 것
+
+    // 파트 2에는 이 아저씨가 만든 Raycast 월드 엔진을 이용해 실제로 게임을 구현해 볼것
+    // 이를 통해 많은 캐릭터가 움직이고 서로 공격하는 것을 가능하게 할것
+
+    return 0;
 
     // 나만의 메세지를 만들고 싶다면 아레의 방법대로 함
     han::net::message<CustomMsgTypes> msg; // 위에서 만든 메세지 타입으로 메세지 객채를 만듬
